5319682bde5699782dc3692e6527ae231dd7b49c
YEVHEN_ZAKHARCH <Y.Zakharchenko@steel.mil.ua>
2024-05-03 12:01:51 +0300

    redo done

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,37 @@ import ProductCard from "@/components/ProductCard.vue";
 import { useProductStore } from "@/stores/ProductStore";
 import { useCartStore } from '@/stores/CartStore'
 // import { after } from "lodash";
-// import { onErrorCaptured } from "vue";
+import { ref, reactive } from "vue";
+
 const productStore = useProductStore();
 const cartStore = useCartStore();
+const history = reactive([])
+const future = reactive([])
+const doingHistory = ref(false)
+
+history.push(JSON.stringify(cartStore.$state))
+const redo = () => {
+  const latestState = future.pop()
+  if(!latestState) return;
+  doingHistory.value = true
+  history.push(latestState)
+  cartStore.$state = JSON.parse(latestState)
+  doingHistory.value = false
+}
+const undo = () => {
+  if(history.length === 1) return
+  doingHistory.value = true
+  future.push(history.pop())
+  cartStore.$state = JSON.parse(history.at(-1))
+  doingHistory.value = false
+}
+cartStore.$subscribe((mutation, state)=> {
+  if(!doingHistory.value){
+    history.push(JSON.stringify(state))
+  }
+  future.splice(0, future.length)
+})
+
 cartStore.$onAction((
   {
     name, store, args, after, onError
@@ -28,6 +56,11 @@ productStore.fill();
 <template>
   <div class="container">
     <TheHeader />
+    <div class='m-5 flex justify-end'>
+      <AppButton @click='undo'>Undo</AppButton>
+      <AppButton class='ml-2' @click='redo'>Redo</AppButton>
+    </div>
+    
     <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
       <ProductCard
         v-for="product in productStore.products"
